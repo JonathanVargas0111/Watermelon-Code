0233784e4940ce1197330ca655d206ba9486afdc
felipe <jonathanfelipe0111@hotmail.com>
2024-02-20 23:44:10 -0500

    metadata home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,10 @@ import 'swiper/css/scrollbar';
 const urbanist = Urbanist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Watter Landing Page",
-  description: "Landing page made by defelipevargas",
-};
+  title: ' Watermelon Code - Desarrollo y Diseño Web en Bogotá',
+  description: 'Watermelon Code es tu socio en desarrollo y diseño web con sede en Bogotá. Especializados en fusionar diseño y funcionalidad para crear experiencias digitales innovadoras y accesibles. ¡Contáctanos para ver nuestros proyectos!',
+  keywords: 'desarrollo web, diseño web, paginas web, aplicaciones web, desarrollo web bogota, diseño web bogota, paginas web bogota, aplicaciones web bogota, colombia',
+}
 
 export default function RootLayout({
   children,
